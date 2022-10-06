70d43eac2861c29211787da301ccc15446c29cb3
CrabbitSlater <crabbitslater@gmail.com>
2022-10-06 13:48:04 +0100

    continuing UI work- made the settings menu appear and dissapear- added SFX and Music buses, and changed the output of all the audiostreams I could find to the relevant bus Started work on pause menu and changed pause behavior to always process- need toa dd an animated widget so that the pause screen doesn't look 'frozen' Also need to figure out how to make tab containers non-clickable as v irritating

--- Assets/PlayButton.gd
+++ Assets/PlayButton.gd
@@ -1,8 +1,3 @@
 extends Button
 
 
-
-
-func _on_PlayButton_pressed():
-	Global.new_game()
-	get_tree().change_scene("res://Assets/Levels/Test_Level.tscn")
---
+++ Assets/TitleMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+func _on_PlayButton_pressed():
+	Global.new_game()
+	get_tree().change_scene("res://Assets/Levels/Test_Level.tscn")
+
+
+func _on_SettingsButton_pressed():
+	$Settings_tabs.visible=true
+
+
+func _on_SettingsCloseButton_pressed():
+	$Settings_tabs.visible=false
