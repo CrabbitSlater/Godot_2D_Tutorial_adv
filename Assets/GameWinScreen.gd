9089e1e27a6b0ab5385268c3e13b65c383d7f535
CrabbitSlater <crabbitslater@gmail.com>
2022-10-27 09:05:02 +0100

    added some internal structure to hiscore manager to check if a score was a highscore or not NECT: sort and append the score- and add popups to win menu

--- Assets/GameWinScreen.gd
+++ Assets/GameWinScreen.gd
@@ -10,5 +10,7 @@ func _ready():
 	$WinScore.set_values("",curr_score[1],curr_score[2],curr_score[3])
 	$WinScore.visible=true
 	
+	if HighScoreManager.is_current_score_hiscore():
+		print("New Highscore- display entername prompt and then suggest display scoretable")
 	#if a highscore prompt for player name and suggest going to highscore screen
 	#else do nothing 
--- HighScoreManager.gd
+++ HighScoreManager.gd
@@ -2,7 +2,7 @@ extends Node
 
 pass
 
-
+const MAX_HISCORES = 10
 const HISCORE_SAVE_FILE = "user://2dtutorial_hiscore.save"
 #check save_file location and that file exists- print to terminal 
 
@@ -14,10 +14,42 @@ var current_score = ["DEBUG",1,2,998]
 var new_hiscore:bool =false
 
 func is_current_score_hiscore():
-	return true
 	
-func add_new_hiscore():
-	pass
+	var lowest_coins:int = 999
+	var low_coins_enemy:int = 999
+	var low_coins_time:int = 999
+	var lowest_enemies:int = 999
+	var low_enemy_coin:int = 999
+	var low_enemy_time:int =999
+	var slowest_run:int = 9999
+	
+	for score in saved_hiscore:
+		if score[1] < lowest_coins:
+			lowest_coins = score[1]
+			low_coins_enemy = score[2]
+			low_coins_time = score[3]
+		if score[2] <lowest_enemies:
+			lowest_enemies = score[2]
+			low_enemy_coin = score[1]
+			low_enemy_time = score[3]
+		if score[3] < slowest_run:
+			slowest_run = score[3]
+			
+	print("least coins is : "+str(lowest_coins))
+	print("least enemies is : "+str(lowest_enemies))
+	print("slowest time is : "+str(slowest_run))
+	
+	#criterion for highscore- 
+	#did you collect more coins than any of the existing hiscore entries? 
+	#	elseif you collected the same as the lowest entry- did you kill more enemies? 
+	#		elseif you killed the same, did you do it faster? 
+	#elseif did you kill more enemies? 
+	#	elseif you killed the same as the lowest entry- did you get more coins?
+	#		elseif you killed the same enemies as the smallest number and the same coins, did you do it faster?
+	
+	#sort priority- coins>enemies>time
+	
+	return true
 
 func _ready():
 	load_hiscores()
@@ -31,6 +63,11 @@ func add_hiscore(name:String, coins:int, enemies:int, time:int):
 	
 	var scorelist = [name,coins,enemies,time]
 	saved_hiscore.append(scorelist)
+	
+	#sort highscores based on criteria in _is_currentscore_hiscore
+	#trim list to MAX_HISCORE entries
+	
+	save_hiscore()
 
 
 
@@ -58,8 +95,7 @@ func load_hiscores():
 		add_hiscore("ass",10,3,300)
 		add_hiscore("longwords",10,2,300)
 		add_hiscore("hellote",10,3,65)
-		
-		save_hiscore()
+		add_hiscore("winky",69,420,65)
 		
 	file.open(HISCORE_SAVE_FILE,File.READ)
 	saved_hiscore = file.get_var()
