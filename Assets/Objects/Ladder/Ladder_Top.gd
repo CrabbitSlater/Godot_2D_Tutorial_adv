fd4f2c89bb2967438211f0e8f6eb8d754d1de60b
CrabbitSlater <crabbitslater@gmail.com>
2022-10-31 21:54:32 +0000

    DONE and DONE finished level, added particles to finish and tweaked some stuff

--- Assets/Objects/Ladder/Ladder_Top.gd
+++ Assets/Objects/Ladder/Ladder_Top.gd
@@ -6,14 +6,15 @@ func _physics_process(delta):
 	if Input.is_action_pressed("player_down") and above_ladder:
 		$CollisionShape2D.rotation_degrees=180
 	else:
-		pass
 		$CollisionShape2D.rotation_degrees=0
 		
 
 
 func _on_AboveChecker_body_entered(body):
 	above_ladder=true
+	print("entered ladder")
 
 
 func _on_AboveChecker_body_exited(body):
 	print("left ladder")
+	above_ladder=false
